81b65c02cc35ed69dda12c5e9f419bdce3a3b658
Amayuda Safei <amayudasafei@gmail.com>
2025-06-04 17:03:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+    menuIcon.classList.toggle('bx-menu');
+    menuIcon.classList.toggle('bx-x');
+});
